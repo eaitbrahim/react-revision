21b04132004430ff7599fb0b50c80a4bbf30bb57
Elmehdi <eaitbrahim@gmail.com>
2021-01-17 13:02:16 -0500

    Setup reducer and prop drilling

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,40 @@
 import React, { useState } from 'react';
+import {data} from '../../../data';
 
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+  const removePerson = (id) => {
+    setPeople(people => people.filter(person => person.id !== id));
+  }
+
+ return (
+  <>
+    <h2>prop drilling</h2>
+    <List people = {people} removePerson={removePerson}/>
+  </>
+  );
+};
+
+const List = ({people, removePerson}) => {
+  return (
+    <>
+      {
+        people.map(person => <SinglePerson key={person.id} {...person} removePerson={removePerson} />)
+      }
+    </>
+  );
+};
+
+const SinglePerson = ({id, name, removePerson}) => {
+  return (
+    <div className="item">
+      <h4>{name}</h4>
+      <button onClick={() => removePerson(id)}>X</button>
+    </div>
+  );
 };
 
 export default PropDrilling;
